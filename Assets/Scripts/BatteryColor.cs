b0296ab9e5c57fbe598d9868c769cb42a0a4fa84
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-11-14 13:53:10 -0400

    Work on interface

---
+++ Assets/Scripts/BatteryColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryColor : MonoBehaviour
+{
+    public Image fillImage;
+    public Color red, yellow, green;
+	public void UpdateColor(float val)
+    {
+        Debug.Log("val changed " + val);
+        Color min, max;
+        float lerp;
+        if (val <= 0.5f)
+        {
+            min = red;
+            max = yellow;
+            lerp = val / 0.5f;
+        }
+        else
+        {
+            min = yellow;
+            max = green;
+            lerp = (val - 0.5f) / 0.5f;
+        }
+        fillImage.color = Color.Lerp(min, max, lerp);
+    }
+}
+
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController controller;
+
+    public Slider p1Battery,
+                  p2Battery;
+
+    public Text p1Score,
+                p2Score,
+                timeLeft,
+                gemsLeft,
+                gameOver,
+                playAgain;
+
+    public Image playAgainButton;
+    
+    void Awake()
+    {
+        if (controller = null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller = this;
+        }
+        else if (controller != this)
+        {
+            Destroy(gameObject);
+        }
+        Reset();
+    }
+
+    void Update()
+    {
+        //if (GameController.controller.gameOver)
+        //{
+            //timeLeft.gameObject.SetActive(false);
+            //gemsLeft.gameObject.SetActive(false);
+            //gameOver.text = "Player " + GameController.controller.winner + " wins!";
+            //gameOver.gameObject.SetActive(true);
+            //playAgain.gameObject.SetActive(true);
+            //playAgainButton.gameObject.SetActive(true);
+        //}
+        //else 
+        //{
+            UpdateTime();
+            UpdateGemsLeft();
+            UpdateScore();
+            UpdateBattery();
+        //}
+    }
+
+    public void Reset()
+    {
+        p1Battery.gameObject.SetActive(true);
+        p1Score.gameObject.SetActive(true);
+        p2Battery.gameObject.SetActive(true);
+        p2Score.gameObject.SetActive(true);
+        timeLeft.gameObject.SetActive(true);
+        gemsLeft.gameObject.SetActive(true);
+        gameOver.gameObject.SetActive(false);
+        playAgain.gameObject.SetActive(false);
+        playAgainButton.gameObject.SetActive(false);
+    }
+
+    public void UpdateTime()
+    {
+        //timeLeft.text = GameController.controller.TimeLeft;
+    }
+
+    public void UpdateGemsLeft()
+    {
+        //gemsLeft.text = GameController.controller.GemsLeft + " Left";
+    }
+
+    public void UpdateScore()
+    {
+        //p1Score.text = GameController.controller.p1Gems;
+        //p2Score.text = GameController.controller.p2Gems;
+    }
+
+    public void UpdateBattery()
+    {
+        //p1Battery.value = GameController.controller.p1Battery;
+        //p2Battery.value = GameController.controller.p2Battery;
+    }
+}
